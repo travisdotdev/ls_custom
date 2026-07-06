9b0bb3dc270ba3c3a6cc509b1e89fa807a7c0334
travisdotdev <travis4yusuf@gmail.com>
2026-07-07 00:05:18 +0100

    Partway done implemenitng -l flag, permissions, group and the user get printed

--- ls.c
+++ ls.c
@@ -2,6 +2,49 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+
+void mode_string(mode_t mode, char *str) {
+	if (S_ISDIR(mode))		  str[0] = 'd';
+	else if (S_ISLNK(mode))   str[0] = 'l';
+	else if (S_ISCHR(mode))	  str[0] = 'c';
+	else if (S_ISBLK(mode))   str[0] = 'b';
+	else if (S_ISFIFO(mode))  str[0] = 'p';
+	else if (S_ISSOCK(mode))  str[0] = 's';
+	else					  str[0] = '-';
+
+	str[1] = (mode & S_IRUSR) ? 'r' : '-';
+	str[2] = (mode & S_IWUSR) ? 'w' : '-';
+	str[3] = (mode & S_IXUSR) ? 'x' : '-';
+	str[4] = (mode & S_IRGRP) ? 'r' : '-';
+	str[5] = (mode & S_IWGRP) ? 'w' : '-';
+	str[6] = (mode & S_IXGRP) ? 'x' : '-';
+	str[7] = (mode & S_IROTH) ? 'r' : '-';
+	str[8] = (mode & S_IWOTH) ? 'w' : '-';
+	str[9] = (mode & S_IXOTH) ? 'x' : '-';
+	str[10] = '\0';
+}
+
+void print_long(const char *dir, const char *name) {
+	char fullpath[4096];
+	snprintf(fullpath,sizeof(fullpath), "%s/%s", dir, name);
+
+	struct stat st;
+	if (lstat(fullpath, &st) < 0) {
+		perror(name);
+		return;
+	}
+	char modes[11];
+	mode_string(st.st_mode, modes);
+
+	struct passwd *pw = getpwuid(st.st_uid);
+	struct group *gr = getgrgid(st.st_uid);
+	const char *user = pw ? pw->pw_name : "?";
+	const char *group = gr ? gr->gr_name : "?";
+	printf("%s %s %s %s\n", modes, user, group, name);
+
+}
 
 int show_all = 0;
 
@@ -30,7 +73,8 @@ int main(int argc, char *argv[]) {
 	struct dirent *entry;
 	while((entry = readdir(dir)) != NULL) {
 		if(!show_all && entry->d_name[0] == '.') continue;
-		printf("%s\n", entry->d_name);
+		print_long(path, entry->d_name);
+		//printf("%s\n", entry->d_name);
 	}
 
 	closedir(dir);
